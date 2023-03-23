3ee28f90a77fdc170f05269c041c98d4275b5212
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-03-23 14:49:14 +0100

    up

--- src/main/java/io/github/chains_project/maven_lockfile/LockFileFacade.java
+++ src/main/java/io/github/chains_project/maven_lockfile/LockFileFacade.java
@@ -33,6 +33,7 @@ import org.eclipse.aether.repository.RepositoryPolicy;
 import org.eclipse.aether.resolution.ArtifactRequest;
 import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
+import org.eclipse.aether.resolution.DependencyResult;
 import org.eclipse.aether.util.artifact.JavaScopes;
 import org.eclipse.aether.util.filter.DependencyFilterUtils;
 
@@ -82,21 +83,14 @@ public class LockFileFacade {
 
         // Get all the artifacts for the dependencies in the project
         for (var artifact : project.getDependencies()) {
-            try {
-                var graph = DependencyGraph.of(
-                        LockFileFacade.generateDependencyGraphForProject(project, repositorySystemSession, repoSystem));
-                var roots = graph.getGraph().stream()
-                        .filter(v -> v.getParent() == null)
-                        .collect(Collectors.toList());
-                return new LockFile(
-                        GroupId.of(project.getGroupId()),
-                        ArtifactId.of(project.getArtifactId()),
-                        VersionNumber.of(project.getVersion()),
-                        roots);
-
-            } catch (DependencyResolutionException e) {
-                new SystemStreamLog().warn("Could not resolve artifact: " + artifact.getArtifactId());
-            }
+            var graph = DependencyGraph.of(LockFileFacade.foo(project, repositorySystemSession, repoSystem));
+            var roots =
+                    graph.getGraph().stream().filter(v -> v.getParent() == null).collect(Collectors.toList());
+            return new LockFile(
+                    GroupId.of(project.getGroupId()),
+                    ArtifactId.of(project.getArtifactId()),
+                    VersionNumber.of(project.getVersion()),
+                    roots);
         }
         return new LockFile(
                 GroupId.of(project.getGroupId()),
@@ -125,43 +119,41 @@ public class LockFileFacade {
                 artifactRequest.setRepositories(list);
                 var result = repoSystem.resolveArtifact(repositorySystemSession, artifactRequest);
                 graph.putEdge(root, result.getArtifact());
-
-            } catch (Exception e) {
-                new SystemStreamLog().warn("Could not resolve artifact: " + defaultArtifact);
-            }
-            try {
                 CollectRequest collectRequest = new CollectRequest();
-
-                collectRequest.setRoot(new Dependency(defaultArtifact, null));
+                collectRequest.setRoot(new Dependency(
+                        result.getArtifact(), result.getArtifact().getClassifier()));
                 collectRequest.setRepositories(list);
-                DependencyFilter classpathFilter =
-                        DependencyFilterUtils.classpathFilter(JavaScopes.TEST, JavaScopes.COMPILE,
-                                JavaScopes.RUNTIME, JavaScopes.SYSTEM, JavaScopes.PROVIDED);
-                DependencyRequest dependencyRequest =
-                        new DependencyRequest(collectRequest, classpathFilter);
-                var nodes =
-                        repoSystem.resolveDependencies(repositorySystemSession, dependencyRequest);
+                DependencyFilter classpathFilter = DependencyFilterUtils.classpathFilter(
+                        JavaScopes.TEST,
+                        JavaScopes.COMPILE,
+                        JavaScopes.RUNTIME,
+                        JavaScopes.SYSTEM,
+                        JavaScopes.PROVIDED);
+                DependencyRequest dependencyRequest = new DependencyRequest(collectRequest, classpathFilter);
 
-                nodes.getRoot().accept(new DependencyVisitor() {
-                    @Override
-                    public boolean visitEnter(DependencyNode node) {
-                        node.getChildren()
-                                .forEach(it -> graph.putEdge(node.getArtifact(), it.getArtifact()));
-                        return true;
-                    }
-
-                    @Override
-                    public boolean visitLeave(DependencyNode node) {
-                        return true;
-                    }
-                });
+                var nodes = repoSystem.resolveDependencies(repositorySystemSession, dependencyRequest);
+                addNodesToGraph(graph, nodes);
                 graph.removeNode(root);
             } catch (Exception e) {
-                new SystemStreamLog().warn("Could not resolve artifact: " + defaultArtifact);
+                new SystemStreamLog().warn("Could not resolve artifact: " + defaultArtifact, e);
             }
-
         }
         return graph;
+    }
+
+    private static void addNodesToGraph(MutableGraph<Artifact> graph, DependencyResult nodes) {
+        nodes.getRoot().accept(new DependencyVisitor() {
+            @Override
+            public boolean visitEnter(DependencyNode node) {
+                node.getChildren().forEach(it -> graph.putEdge(node.getArtifact(), it.getArtifact()));
+                return true;
+            }
+
+            @Override
+            public boolean visitLeave(DependencyNode node) {
+                return true;
+            }
+        });
     }
 
     public static List<RemoteRepository> newRepositories() {
@@ -172,5 +164,88 @@ public class LockFileFacade {
         return new RemoteRepository.Builder("central", "default", "https://repo.maven.apache.org/maven2/")
                 .setPolicy(new RepositoryPolicy(true, "always", RepositoryPolicy.CHECKSUM_POLICY_FAIL))
                 .build();
+    }
+
+    public static Graph<Artifact> foo(
+            MavenProject project, RepositorySystemSession repositorySystemSession, RepositorySystem repoSystem) {
+        MutableGraph<Artifact> graph = GraphBuilder.directed().build();
+        var list = newRepositories();
+        // there is a feature in maven where it will not resolve dependencies from http repositories
+        list.addAll(project.getRemoteProjectRepositories().stream()
+                .filter(v -> v.getUrl().contains("https"))
+                .collect(Collectors.toList()));
+        for (var dep : project.getDependencies()) {
+            try {
+
+                CollectRequest collectRequest = new CollectRequest();
+                collectRequest.setRoot(new Dependency(dependencyToArtifact(dep), dep.getScope()));
+                collectRequest.setRepositories(list);
+                var result = repoSystem.collectDependencies(repositorySystemSession, collectRequest);
+                Visitor visitor = new Visitor(repositorySystemSession, repoSystem, graph);
+                var root = result.getRoot();
+                root.accept(visitor);
+            } catch (Exception e) {
+                new SystemStreamLog().warn("Could not resolve artifact: " + dep.getArtifactId(), e);
+            }
+        }
+        return graph;
+    }
+
+    private static Artifact dependencyToArtifact(org.apache.maven.model.Dependency dependency) {
+        return new DefaultArtifact(
+                dependency.getGroupId(), dependency.getArtifactId(), dependency.getType(), dependency.getVersion());
+    }
+
+    private static class Visitor implements DependencyVisitor {
+
+        private RepositorySystemSession session;
+        private RepositorySystem repoSystem;
+        private MutableGraph<Artifact> graph;
+
+        public Visitor(RepositorySystemSession session, RepositorySystem repoSystem, MutableGraph<Artifact> graph) {
+            this.session = session;
+            this.repoSystem = repoSystem;
+            this.graph = graph;
+        }
+
+        @Override
+        public boolean visitEnter(DependencyNode node) {
+            var artifact = resolveArtifact(node);
+            node.getChildren().forEach(it -> graph.putEdge(artifact, resolveArtifact(it)));
+            return true;
+        }
+
+        @Override
+        public boolean visitLeave(DependencyNode node) {
+            return true;
+        }
+
+        private Artifact resolveArtifact(DependencyNode node) {
+            try {
+                ArtifactRequest artifactRequest = new ArtifactRequest();
+                artifactRequest.setArtifact(node.getArtifact());
+                artifactRequest.setRepositories(node.getRepositories());
+                var result = repoSystem.resolveArtifact(session, artifactRequest);
+                return result.getArtifact();
+            } catch (Exception e) {
+                try {
+                    ArtifactRequest artifactRequest = new ArtifactRequest();
+                    artifactRequest.setArtifact(new DefaultArtifact(
+                            node.getArtifact().getGroupId(),
+                            node.getArtifact().getArtifactId(),
+                            node.getArtifact().getClassifier(),
+                            "pom",
+                            node.getArtifact().getVersion()));
+                    artifactRequest.setRepositories(node.getRepositories());
+                    var result = repoSystem.resolveArtifact(session, artifactRequest);
+                    return result.getArtifact();
+                } catch (Exception inner) {
+                    new SystemStreamLog().warn("Could not resolve artifact: " + node.getArtifact(), inner);
+                }
+                new SystemStreamLog().warn("Could not resolve artifact: " + node.getArtifact(), e);
+            }
+            // fallback to the original artifact
+            return node.getArtifact();
+        }
     }
 }
