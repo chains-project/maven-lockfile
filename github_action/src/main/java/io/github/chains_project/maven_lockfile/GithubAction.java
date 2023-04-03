9b05cba8820a9e8edbf983bbe790d4dadf4f28de
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-04-03 11:48:02 +0200

    ✨ Use changed Files action instead of jgit (#105)

--- github_action/src/main/java/io/github/chains_project/maven_lockfile/GithubAction.java
+++ github_action/src/main/java/io/github/chains_project/maven_lockfile/GithubAction.java
@@ -4,37 +4,23 @@ import io.quarkiverse.githubaction.Action;
 import io.quarkiverse.githubaction.Commands;
 import io.quarkiverse.githubaction.Context;
 import io.quarkiverse.githubaction.Inputs;
-import java.io.IOException;
-import java.nio.file.Path;
 import org.buildobjects.process.ProcBuilder;
-import org.eclipse.jgit.api.Git;
 import org.kohsuke.github.GitHub;
 
 public class GithubAction {
 
-    private final GitUtils gitUtils;
     private static final String COMMAND_GENERATE = "io.github.chains-project:maven-lockfile:1.0.8-SNAPSHOT:generate";
     private static final String COMMAND_VALIDATE = "io.github.chains-project:maven-lockfile:1.0.8-SNAPSHOT:validate";
 
-    public GithubAction(GitUtils gitUtils) {
-        this.gitUtils = gitUtils;
+
+    @Action("generate")
+    void runLockFile(Inputs inputs, Commands commands, Context context, GitHub gitHub) {
+        new ProcBuilder("mvn").withNoTimeout().withArg(COMMAND_GENERATE).run();
     }
 
-    @Action
-    void runLockFile(Inputs inputs, Commands commands, Context context, GitHub gitHub) throws IOException {
-        String baseRef = context.getGitHubBaseRef();
-        String headRef = context.getGitHubRef();
-        Path repo = Path.of(context.getGitHubWorkspace());
-        try (Git git = Git.open(repo.toFile())) {
-            if (gitUtils.isPomChanged(repo, baseRef, headRef)) {
-                new ProcBuilder("mvn").withNoTimeout().withArg(COMMAND_GENERATE).run();
-            } else {
-                validateLockFile(commands);
-            }
-        } catch (Exception e) {
-            commands.error(e.getMessage());
-            System.exit(1);
-        }
+    @Action("validate")
+    void validateLockFile(Inputs inputs, Commands commands, Context context, GitHub gitHub) {
+        validateLockFile(commands);
     }
 
     private void validateLockFile(Commands commands) {
@@ -52,7 +38,7 @@ public class GithubAction {
             commands.error(
                     "Integrity check failed\n Please run `mvn io.github.chains-project:maven-lockfile:0.3.2:generate` and commit the changes."
                             + e.getMessage());
-            System.exit(-1);
+            System.exit(1);
         }
     }
 }
