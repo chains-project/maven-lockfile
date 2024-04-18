b5608228f4eef57e6141afb37be7deaded1d857a
yogyagamage <yogyagamage@gmail.com>
2024-04-18 15:54:10 +0200

    Update readme and fix imports

--- maven_plugin/src/main/java/io/github/chains_project/maven_lockfile/FreezeDependencyMojo.java
+++ maven_plugin/src/main/java/io/github/chains_project/maven_lockfile/FreezeDependencyMojo.java
@@ -2,11 +2,6 @@ package io.github.chains_project.maven_lockfile;
 
 import io.github.chains_project.maven_lockfile.data.LockFile;
 import io.github.chains_project.maven_lockfile.graph.DependencyNode;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.*;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.DependencyManagement;
@@ -21,6 +16,12 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Freeze the dependencies of a project. Every dependency will be locked to a specific version.
