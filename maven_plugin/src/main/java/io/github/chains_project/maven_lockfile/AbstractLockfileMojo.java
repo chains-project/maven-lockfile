0f98687832ba73884832d511868cdb099407f147
Elias Lundell <36220731+LogFlames@users.noreply.github.com>
2025-11-03 12:29:22 +0100

    🦺 fix: Force local checksum mode on validate (#1282)
    Co-authored-by: Martin Monperrus <martin.monperrus@gnieh.org>

--- maven_plugin/src/main/java/io/github/chains_project/maven_lockfile/AbstractLockfileMojo.java
+++ maven_plugin/src/main/java/io/github/chains_project/maven_lockfile/AbstractLockfileMojo.java
@@ -96,10 +96,20 @@ public abstract class AbstractLockfileMojo extends AbstractMojo {
     }
 
     protected AbstractChecksumCalculator getChecksumCalculator(Config config) throws MojoExecutionException {
+        return getChecksumCalculator(config, false);
+    }
+
+    protected AbstractChecksumCalculator getChecksumCalculator(Config config, boolean forceLocalChecksumMode)
+            throws MojoExecutionException {
         ProjectBuildingRequest artifactBuildingRequest = newResolveArtifactProjectBuildingRequest();
         ProjectBuildingRequest pluginBuildingRequest = newResolvePluginProjectBuildingRequest();
 
-        switch (checksumModeDeprecation(config.getChecksumMode())) {
+        String checksumMode = checksumModeDeprecation(config.getChecksumMode());
+        if (forceLocalChecksumMode) {
+            checksumMode = "local";
+        }
+
+        switch (checksumMode) {
             case "local":
                 return new FileSystemChecksumCalculator(
                         dependencyResolver,
@@ -110,7 +120,7 @@ public abstract class AbstractLockfileMojo extends AbstractMojo {
                 return new RemoteChecksumCalculator(
                         config.getChecksumAlgorithm(), artifactBuildingRequest, pluginBuildingRequest);
             default:
-                throw new MojoExecutionException("Invalid checksum mode: " + config.getChecksumMode());
+                throw new MojoExecutionException("Invalid checksum mode: " + checksumMode);
         }
     }
 
